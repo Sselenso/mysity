989210d48ec573984f9e4e7d55361ca5d5b8b26b
Sselenso <122260649+Sselenso@users.noreply.github.com>
2024-01-14 16:57:15 +0300

    init

---
+++ assets/js/app.js
@@ -0,0 +1,70 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+if (ScrollTrigger.isTouch !== 1) {
+
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true
+	})
+
+	gsap.fromTo('.header__logo', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.hero-section',
+			start: 'center',
+			end: '820',
+			scrub: true
+		}
+	})
+
+	let itemsA = gsap.utils.toArray('.tarif__sale .tarif__sale-block')
+
+	itemsA.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, y: -100 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+
+	let itemsL = gsap.utils.toArray('.tarif__left .tarif-block')
+
+	itemsL.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: -50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+
+	let itemsR = gsap.utils.toArray('.tarif__right .tarif-block')
+
+	itemsR.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: 50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-750',
+				end: 'top',
+				scrub: true
+			}
+		})
+	})
+
+}
+
+ScrollSmoother.create({
+  smooth: 1.5, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
